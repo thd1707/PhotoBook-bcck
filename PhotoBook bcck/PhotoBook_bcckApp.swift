55bd4b6241ff6fe5ac2af19bda768606a37e42a7
Hong Duc <tranhongduchkn@gmail.com>
2025-09-06 23:50:27 +0700

    Initial Commit

---
+++ PhotoBook bcck/PhotoBook_bcckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBook_bcckApp.swift
+//  PhotoBook bcck
+//
+//  Created by TY on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBook_bcckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
